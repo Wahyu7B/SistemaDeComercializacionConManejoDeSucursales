417c4147901d6c9043ba0e243fc30d421a99ba24
lucixSSJ <franzsanzz2@gmail.com>
2025-10-10 10:25:19 -0500

    SrpingBoot

---
+++ ProyectoJS/src/main/java/com/Proyecto_JS/ProyectoJS/ProyectoJsApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto_JS.ProyectoJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJsApplication.class, args);
+	}
+
+}
---
+++ ProyectoJS/src/test/java/com/Proyecto_JS/ProyectoJS/ProyectoJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_JS.ProyectoJS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
